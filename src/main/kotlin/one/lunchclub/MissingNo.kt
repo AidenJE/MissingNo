e77fca598595212214737d67981a28abbfba0d9e
AidenJE <majoraaj@gmail.com>
2021-10-07 20:49:52 -0400

    give carlos insane knockback powers

---
+++ src/main/kotlin/one/lunchclub/MissingNo.kt
@@ -0,0 +1,12 @@
+package one.lunchclub
+
+import one.lunchclub.listener.CarlosListener
+import org.bukkit.plugin.java.JavaPlugin
+
+class MissingNo : JavaPlugin() {
+	override fun onEnable() {
+		saveDefaultConfig()
+
+		server.pluginManager.registerEvents(CarlosListener(this), this)
+	}
+}
---
+++ src/main/kotlin/one/lunchclub/listener/CarlosListener.kt
@@ -0,0 +1,34 @@
+package one.lunchclub.listener
+
+import one.lunchclub.MissingNo
+import org.bukkit.Material
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.entity.EntityDamageByEntityEvent
+import org.bukkit.event.entity.EntityDamageEvent
+
+class CarlosListener(private val plugin: MissingNo) : Listener {
+	private fun Player.isCarlos(): Boolean {
+		val carlosUniqueId = "5aac6576-86f7-468a-8f27-2d3c3aac74d7"
+		val playerUniqueId = uniqueId.toString()
+
+		return playerUniqueId == carlosUniqueId
+	}
+
+	@EventHandler
+	fun onAttack(event: EntityDamageByEntityEvent) {
+		val attacker = event.damager
+		val victim = event.entity
+
+		if (attacker is Player) {
+			val isPlayerHoldingNothing = attacker.inventory.itemInMainHand.type == Material.AIR
+			val isPlayerAttacking = event.cause == EntityDamageEvent.DamageCause.ENTITY_ATTACK
+
+			if (attacker.isCarlos() && isPlayerHoldingNothing && isPlayerAttacking) {
+				val knockback = plugin.config.getDouble("carlos.knockback")
+				victim.velocity = attacker.location.direction.normalize().multiply(knockback)
+			}
+		}
+	}
+}
